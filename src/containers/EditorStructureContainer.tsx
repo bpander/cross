e2dd1440ba3f608fc92228673ff73d856373fc24
Brad Anderson <brad.anderson87@gmail.com>
2018-12-28 14:15:48 -0500

    Add a couple more icons.

--- src/containers/EditorStructureContainer.tsx
+++ src/containers/EditorStructureContainer.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import Icon from 'icons/Icon';
-import iconUndo from 'icons/iconUndo';
-import iconRedo from 'icons/iconRedo';
 import iconChecker from 'icons/iconChecker';
-import iconSquare from 'icons/iconSquare';
 import iconCircle from 'icons/iconCircle';
+import iconCursor from 'icons/iconCursor';
+import iconRedo from 'icons/iconRedo';
+import iconSquare from 'icons/iconSquare';
+import iconTextRotationNone from 'icons/iconTextRotationNone';
+import iconUndo from 'icons/iconUndo';
 
 class EditorStructureContainer extends React.Component {
 
@@ -27,6 +29,12 @@ class EditorStructureContainer extends React.Component {
         </button>
         <button className="btn">
           <Icon def={iconCircle} className="d-block" />
+        </button>
+        <button className="btn">
+          <Icon def={iconTextRotationNone} className="d-block" />
+        </button>
+        <button className="btn">
+          <Icon def={iconCursor} className="d-block" />
         </button>
       </div>
     );
