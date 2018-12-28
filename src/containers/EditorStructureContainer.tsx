a8eb92191b883562bf81c94f9ffc514d55a02632
Brad Anderson <brad.anderson87@gmail.com>
2018-12-28 16:53:59 -0500

    Refine structure toolbar styles.

--- src/containers/EditorStructureContainer.tsx
+++ src/containers/EditorStructureContainer.tsx
@@ -14,28 +14,47 @@ class EditorStructureContainer extends React.Component {
 
   renderToolbar() {
     return (
-      <div className="py-2">
-        <button className="btn">
-          <Icon def={iconUndo} className="d-block" />
-        </button>
-        <button className="btn" disabled>
-          <Icon def={iconRedo} className="d-block" />
-        </button>
-        <button className="btn btn--active">
-          <Icon def={iconChecker} className="d-block" />
-        </button>
-        <button className="btn">
-          <Icon def={iconSquare} className="d-block" />
-        </button>
-        <button className="btn">
-          <Icon def={iconCircle} className="d-block" />
-        </button>
-        <button className="btn">
-          <Icon def={iconTextRotationNone} className="d-block" />
-        </button>
-        <button className="btn">
-          <Icon def={iconCursor} className="d-block" />
-        </button>
+      <div className="py-1 typ-1.5">
+        <ul className="h-list h-list-1">
+          <li>
+            <button className="btn d-block">
+              <Icon def={iconUndo} className="d-block" />
+            </button>
+          </li>
+          <li>
+            <button className="btn d-block" disabled>
+              <Icon def={iconRedo} className="d-block" />
+            </button>
+          </li>
+          <li>
+            <div className="vr mx-1" />
+          </li>
+          <li>
+            <button className="btn btn--active d-block">
+              <Icon def={iconChecker} className="d-block" />
+            </button>
+          </li>
+          <li>
+            <button className="btn d-block">
+              <Icon def={iconSquare} className="d-block" />
+            </button>
+          </li>
+          <li>
+            <button className="btn d-block">
+              <Icon def={iconCircle} className="d-block" />
+            </button>
+          </li>
+          <li>
+            <button className="btn d-block">
+              <Icon def={iconTextRotationNone} className="d-block" />
+            </button>
+          </li>
+          <li>
+            <button className="btn d-block">
+              <Icon def={iconCursor} className="d-block" />
+            </button>
+          </li>
+        </ul>
       </div>
     );
   }
